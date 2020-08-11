64ca6fa48ea7cc92a9b754446540f251b3fbb4a4
rutuja-2803 <67337174+rutuja-2803@users.noreply.github.com>
2020-08-11 22:35:52 +0530

    Add files via upload

---
+++ day9.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the factorial function below.
+def factorial(n):
+    result=1
+    for x in range(1,n+1):
+        result=result*x
+    return result    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    result = factorial(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
